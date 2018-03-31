d6e77d96f7309775519de89cefebf1ffa781bc36
fermi_dirac <ManueleBryan@gmail.com>
2018-03-31 12:06:15 -0700

    working on front end

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './../assets/logo.svg';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello World
+      </div>
+    );
+  }
+}
+
+export default App;
